89f8424a5427d16bfbcec73a42c5f5d7c7de0e57
kyungh103 <kyungh103@naver.com>
2019-07-24 11:03:06 +0900

    .

---
+++ src/main/resources/jython/imgPredict.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+#def imgTest(imgpath):     
+imgpath = "C:/python_ML/img/7.png"
+imgBGR = cv2.imread(imgpath)
+
+img_numpy = np.array(imgBGR, 'uint8')
+imgGRAY = cv2.cvtColor(img_numpy, cv2.COLOR_BGR2GRAY)
+print(imgGRAY)
