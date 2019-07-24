82670ed997c15ac0a342699670fbf448f070b64f
student <student@M150128>
2019-07-24 11:24:42 +0900

    test2.py

--- src/main/resources/jython/test2.py
+++ src/main/resources/jython/test2.py
@@ -1,10 +1,12 @@
 import cv2
-import numpy as np
 
 #def imgTest(imgpath): 
 imgpath = "C:/python_ML/img/7.png"
-imgBGR = cv2.imread(imgpath)
 
-img_numpy = np.array(imgBGR, 'uint8')
-imgGRAY = cv2.cvtColor(img_numpy, cv2.COLOR_BGR2GRAY)
-print(imgGRAY)
+
+gray = cv2.imread(imagePath, cv2.IMREAD_GRAYSCALE)
+gray = cv2.resize(255-gray,(28,28))
+test_img = gray.flatten()/255.0
+test_img = test_img.reshape((-1,28,28,1))
+
+print(test_img)
